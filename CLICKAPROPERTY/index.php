8225fca22eda4e1f66dd918e951537c171a4bad9
Settykaz <settykaz07@gmail.com>
2017-06-07 16:38:27 +0200

    Add files via upload
    ClickAproperty project

---
+++ CLICKAPROPERTY/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>ClickAproperty.com</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+   
+
+	<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+<script type="text/javascript" src="engine0/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+
+</head>
+
+                <div id="logo" >
+                    <img src="images/logg.png" id="Image1">
+                </div>
+
+                <div id="social-media">
+                <input type="text" name="search" placeholder="Search anything here">
+
+
+                <!--<p><img src="images/Call.png" width="30px" height="30px" ><b>+263.444.444 or 067.767</b>-->
+
+                 <ul>
+                    <li> <a href="http://www.facebook.com"> <img src="images/fb.png" width="30px" height="30px"></a></li>
+                    <li> <a href="http://www.twitter.com"> <img src="images/intgrm.png" width="30px" height="30px"></a></li>
+                    <li> <a href="http://www.facebook.com"> <img src="images/yt.png" width="30px" height="30px"></a></li>
+                    <li> <a href="http://www.facebook.com"> <img src="images/tw.png" width="30px" height="30px"></a></li>
+                 </ul>
+                 </div>
+
+        <div id="menu">
+            <ul>
+                <li><a href="main.html">HOME</a></li>
+                <li><a href="services.html">RESIDENTIAL</a>
+                    <ul>
+                        <li><a href="#">Low Density</a></li>
+                        <li><a href="#">Medium </a></li>
+                        <li><a href="#">High Density</a></li>
+                    </ul>
+
+                </li>
+                
+                <li><a href="partners.html">COMMERCIAL</a>
+                        <ul>
+                        <li><a href="#">CBD</a></li>
+                        <li><a href="#">OUTSKIRTS</a></li>
+                        
+                    </ul>
+
+                </li>
+                <li><a href="clients.html">INDUSTRIAL</a></li>
+                <li><a href="faqs.html">FAQs</a></li>
+                <li><a href="about.html">ABOUT US</a>
+                        <ul>
+                        <li><a href="#">Vision</a></li>
+                        <li><a href="#">Mission</a></li>
+                        <li><a href="#">Objectives</a></li>
+                    </ul>
+                </li>
+                <li><a href="contacts.html">CONTACTS</a></li>
+            </ul>
+                </div>
+        </div>
+
+
+    <div id="banner">
+	<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container0">
+<div class="ws_images"><ul>
+        <li><img src="data0/images/driveway.jpg" alt="Driveway" title="Driveway" id="wows0_0"/></li>
+        <li><img src="data0/images/exquisite_designs.png" alt="Exquisite Designs" title="Exquisite Designs" id="wows0_1"/></li>
+        <li><img src="data0/images/high_density.jpg" alt="High Density" title="High Density" id="wows0_2"/></li>
+        <li><img src="data0/images/low_density.jpg" alt="Low Density" title="Low Density" id="wows0_3"/></li>
+        <li><img src="data0/images/luxurious_life.jpg" alt="Luxurious Life" title="Luxurious Life" id="wows0_4"/></li>
+        <li><img src="data0/images/medium_density.jpg" alt="Medium Density" title="Medium Density" id="wows0_5"/></li>
+        <li><img src="data0/images/zimreparkhouse.jpg" alt="Zimre-Park-House" title="Zimre-Park-House" id="wows0_6"/></li>
+        <li><a href="http://wowslider.com/vi"><img src="data0/images/zimreparkhouses.jpg" alt="slider" title="Zimre-Park-Houses" id="wows0_7"/></a></li>
+        <li><img src="data0/images/zimreparkhousess.jpg" alt="Zimre-Park-Housess" title="Zimre-Park-Housess" id="wows0_8"/></li>
+    </ul></div>
+    <div class="ws_bullets"><div>
+        <a href="#" title="Driveway"><span><img src="data0/tooltips/driveway.jpg" alt="Driveway"/>1</span></a>
+        <a href="#" title="Exquisite Designs"><span><img src="data0/tooltips/exquisite_designs.png" alt="Exquisite Designs"/>2</span></a>
+        <a href="#" title="High Density"><span><img src="data0/tooltips/high_density.jpg" alt="High Density"/>3</span></a>
+        <a href="#" title="Low Density"><span><img src="data0/tooltips/low_density.jpg" alt="Low Density"/>4</span></a>
+        <a href="#" title="Luxurious Life"><span><img src="data0/tooltips/luxurious_life.jpg" alt="Luxurious Life"/>5</span></a>
+        <a href="#" title="Medium Density"><span><img src="data0/tooltips/medium_density.jpg" alt="Medium Density"/>6</span></a>
+        <a href="#" title="Zimre-Park-House"><span><img src="data0/tooltips/zimreparkhouse.jpg" alt="Zimre-Park-House"/>7</span></a>
+        <a href="#" title="Zimre-Park-Houses"><span><img src="data0/tooltips/zimreparkhouses.jpg" alt="Zimre-Park-Houses"/>8</span></a>
+        <a href="#" title="Zimre-Park-Housess"><span><img src="data0/tooltips/zimreparkhousess.jpg" alt="Zimre-Park-Housess"/>9</span></a>
+    </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com/vi">slider</a> by WOWSlider.com v8.7</div>
+<div class="ws_shadow"></div>
+</div>  
+<script type="text/javascript" src="engine0/wowslider.js"></script>
+<script type="text/javascript" src="engine0/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+</div>
+       
+
+       <div id="colum_info">
+
+            <div id="c_header">
+                <p>GET STARTED</p>
+            </div>
+
+           <div id="colum1">
+                <img src="images/b.png" height="150" width="150">
+               
+               <p>PURCHASE OR RENT<p>
+           </div>
+
+           
+           <div id="colum3">
+           <img src="images/c.png" height="150" width="150">
+               <p>PAYMENT TERMS</p>
+           </div>
+
+           <div id="colum2">
+           <img src="images/a.png" height="140px" width="150px">
+               <p>SEARCH A HOME</p>
+
+           </div>
+
+
+
+       </div>
+
+       <div id="moreinfo">
+            <div id="raper">
+                
+           
+            <div id="words">
+                
+                    <h3>WHAT WE DO </h3><p>
+                    We offer an easy to navigate home search experience, whether you are at home or on the go. We make it easy for you to access our vast network of real estate listings.</p>
+
+                
+            </div>
+            <div id="words2">
+                
+                    <h3>OUR MANDATE</h3><p> <br>
+                    We provide the service through our database of connection buyers and sellers to   all the registered real estate agents in Zimbabwe.</p>
+                
+            </div>
+
+             </div>
+           <div id="pic1">
+             <div id="container">
+            <img src="images/one.jpg" alt="Avatar" id="image">
+            <div id="overlay">
+            <div id="text"><b>Newly Built</b><br>$15 500<br><b>Rent</b><br>$300/month</div>
+            </div>
+            </div>
+           </div>
+
+           <div id="pic2">
+              <div id="container">
+            <img src="images/thre.jpg" alt="Avatar" id="image">
+            <div id="overlay">
+            <div id="text">Borrowdale <br>Zimbabwe</div>
+            </div>
+            </div>
+            </div>
+
+
+           <div id="pic3">
+               <div id="container">
+            <img src="images/three.jpg" alt="Avatar" id="image">
+            <div id="overlay">
+            <div id="text">Mandara Zimbabwe</div>
+            </div>
+            </div>
+           </div>
+
+           <div id="pic4">
+            <div id="container">
+            <img src="images/ten.jpg" alt="Avatar" id="image">
+            <div id="overlay">
+            <div id="text">Zimre Park Harare<br>Available:New <b>$20 000</b></div>
+            </div>
+            </div>
+           </div>
+              
+
+           <div id="pic5">
+              <div id="container">
+            <img src="images/image-1.jpg" alt="Avatar" id="image">
+            <div id="overlay">
+            <div id="text">Industrial Site<br><b>Bulawayo</b></div>
+            </div>
+            </div>
+            </div>
+
+            </div>
+
+       <div id="other">
+           <div id="mediaa">
+               <div id="med_header">
+                   <h3>MEDIA</h3>
+               </div>
+               <div id="con">
+                   <video src="media/1.mp4" controls="ALL" width="460" height="300"></video>
+               </div>
+           </div>
+
+           <div id="locationss">
+           
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.1031823823837!2d30.19110711430729!3d-17.358767288095777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1930b4edf4f3541b%3A0xe123c8d72e6f46f3!2sCABS!5e0!3m2!1sen!2szw!4v1496664525960" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+           </div>
+       </div>
+
+
+        <div id="footer">
+
+            <div id="qlinks">
+            <h4>QUICKLINKS</h4>
+                <ul>
+                    <li><a href="#">About Us</a>|</li>
+                    <li><a href="#">Vision</a>|</li>
+                    <li><a href="#">Mission Statement</a>|</li>
+                    <li><a href="#">Our Ethos</a>|</li>
+                    <li><a href="#">Our Clients</a>|</li>
+                </ul>
+            </div>
+
+            <div id="cals">
+            
+                <img src="images/fooo.jpg" height="200px">
+            </div>
+
+            <div id="copyr">
+                <p>&copy;COPYRIGHT 2017 &bull; ALL RIGHTS RESERVED  &bull; CLICKAPROPERTY ZIMBABWE  &bull; clickaproperty&#64;gmail.com&reg;&trade; </p>
+            </div>
+            
+        </div>
+        </div>
+
+    </body>
+</html>
